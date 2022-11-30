e9a2c611cd885ddd1b38a587ac4386236fc6dcd3
Jose Villar <jose.villar@notacool.com>
2022-11-30 12:30:15 +0100

    cambios de produccion

---
+++ build/precache-manifest.b28bb03935ae6094a4514e6b37cb15e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9d5c0e381579bf5ca03d6fac6d12ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "937ae113fb8844d18ec1",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "9c7b83cbf4dc66b0824e",
+    "url": "/static/css/main.6d45336a.chunk.css"
+  },
+  {
+    "revision": "937ae113fb8844d18ec1",
+    "url": "/static/js/2.e6b490d8.chunk.js"
+  },
+  {
+    "revision": "50fd868c21db533705cd6c45173fab95",
+    "url": "/static/js/2.e6b490d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7b83cbf4dc66b0824e",
+    "url": "/static/js/main.0ad0f449.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
